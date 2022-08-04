3bf0008ddba48fadb8407dec9a3f4e1b6ab18843
Luis Antonio Garcia Perrusquia <lgarciaperrusquia@gmail.com>
2022-08-04 16:46:32 -0500

    reactiveFormCss
    Se crea de manera manual los estilos de rrro de reactrives forms

--- frontend/src/app/modules/beneficiario/personas/utilsPersonas/contactosPersona/contactos.component.ts
+++ frontend/src/app/modules/beneficiario/personas/utilsPersonas/contactosPersona/contactos.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTable } from '@angular/material/table';
+import { promises, resolve } from 'dns';
 
 @Component({
     selector: 'app-contactos',
@@ -84,25 +85,57 @@ export class ContactosComponent implements OnInit, OnDestroy {
     };
 
     setManualError = (currentIdContacto, idError, textDiv) => {
-        setTimeout(() => {
-            if (document.getElementById('divError')) {
-                document.getElementById('divError').remove();
-            };
-            let element = document.getElementById(currentIdContacto);
-            element.classList.add('errorForm');
-            if (idError !== '') {
-                if (!document.getElementById('divError')) {
-                    const errorDiv = document.createElement('div');
-                    errorDiv.setAttribute('id', 'divError');
-                    errorDiv.style.marginTop = "-17px";
-                    errorDiv.style.fontSize = "13px";
-                    errorDiv.style.color = "#DC2626";
-                    errorDiv.textContent = textDiv;
-                    let elementTipo = document.getElementById(idError);
-                    elementTipo.appendChild(errorDiv);
+        setTimeout(async () => {
+            for (let contacto of this.arrayAllContactos) {
+                let idDivTipo = `${contacto.id}_divTipo`;
+                let idDivDato = `${contacto.id}_divDato`;
+                let idPadreTipo = `${contacto.id}_tipo`;
+                let idPadreDato = `${contacto.id}_dato`;
+                let idComponentPadreTipo = `${contacto.id}_tipo_component`;
+                let idComponentPadreDato = `${contacto.id}_dato_component`;
+
+
+                if (document.getElementById(idDivTipo)) {
+                    document.getElementById(idDivTipo).remove();
+                };
+                if (document.getElementById(idDivDato)) {
+                    document.getElementById(idDivDato).remove();
+                };
+
+                if (JSON.stringify(contacto.data) === '{}') {
+                    await this.createAndEmbebedDivError(idDivTipo, idPadreTipo, idComponentPadreTipo, 'Selecciona el tipo de contacto');
+                    await this.createAndEmbebedDivError(idDivDato, idPadreDato, idComponentPadreDato, 'Ingresa el dato');
+                } else {
+                    if (contacto.data.idTipCont === 0 || contacto.data.idTipCont === undefined || contacto.data.idTipCont === null) {
+                        await this.createAndEmbebedDivError(idDivTipo, `${contacto.id}_tipo`, idComponentPadreTipo, 'Selecciona el tipo de contacto');
+                    };
+                    if (contacto.data.dato === '' || contacto.data.dato === undefined || contacto.data.dato === null) {
+                        await this.createAndEmbebedDivError(idDivDato, `${contacto.id}_dato`, idComponentPadreDato, 'Ingresa el dato');
+                    };
                 };
             };
         }, 50);
+    };
+
+    createAndEmbebedDivError = (idDiv, idPadre, componentePadre, textDiv) => {
+        return new Promise(resolve => {
+            //agregamos el texto bajo el div
+            const errorDiv = document.createElement('div');
+            errorDiv.setAttribute('id', idDiv);
+            errorDiv.style.marginTop = "-17px";
+            errorDiv.style.fontSize = "13px";
+            errorDiv.style.color = "#DC2626";
+            errorDiv.textContent = textDiv;
+            let element = document.getElementById(idPadre);
+            element.appendChild(errorDiv);
+
+            //Agregamos para que se va el pintado
+            let component = document.getElementById(componentePadre)
+            component.classList.add('mat-form-field-invalid');
+            component.classList.add('ng-touched');
+
+            resolve({ success: 1 })
+        });
     };
 
     redirect(url: string) {
